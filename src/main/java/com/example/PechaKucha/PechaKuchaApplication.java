643a152366b50f716ec5dcf146e0cff4a2119089
lokicherry <rlokesh@aconex.com>
2018-01-11 10:49:37 +0530

    Initializing project woth spring initializer

---
+++ src/main/java/com/example/PechaKucha/PechaKuchaApplication.java
@@ -0,0 +1,12 @@
+package com.example.PechaKucha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PechaKuchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PechaKuchaApplication.class, args);
+	}
+}
